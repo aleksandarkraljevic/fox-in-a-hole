ba9fef289b1cbedf384f2080002c9aac260d4065
aleksandarkraljevic <a.kraljevic.2000@hotmail.com>
2023-09-18 19:02:13 +0200

    Fixed the code for the evalution of the model

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,11 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from collections import deque
-from tqdm import tqdm
-import time
-import random
 import matplotlib.pyplot as plt
-
 from fox_in_a_hole import *
 from helper import *
 
@@ -17,6 +12,13 @@ def evaluate(model_name, n_samples, n_holes, print_strategy):
     won, lost = done
     current_episode_length = 0
     episode_lengths = []
+    if print_strategy:
+        for step in range(len(observation)):
+            predicted_q_values = model.predict(np.asarray(observation).reshape(1, 2 * n_holes), verbose=0)
+            action = np.argmax(predicted_q_values) + 1
+            observation[step] = action
+        print(observation)
+        observation = [0] * (2 * n_holes)
     for sample in range(n_samples):
         while (not won) and (not lost):
             current_episode_length += 1
@@ -27,15 +29,14 @@ def evaluate(model_name, n_samples, n_holes, print_strategy):
             new_observation = observation.copy()
             new_observation[current_episode_length - 1] = action
             observation = new_observation
+            fox = env.step()
             if won or lost:
-                if print_strategy:
-                    print(observation)
                 episode_lengths.append(current_episode_length)
                 current_episode_length = 0
                 fox, done = env.reset()
+                won, lost = done
                 observation = [0] * (2 * n_holes)
                 break
-            fox = env.step()
     print('The average amount of guesses needed to finish the game is: ',np.mean(episode_lengths))
 
 evaluate(model_name='test.keras',n_samples=1000,n_holes=5,print_strategy=True)
