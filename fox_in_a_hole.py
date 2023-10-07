Recoded the model and environment to look for lowest average amount of guesses instead of the sequence. Also fixed some reward distribution bugs.

fox_in_a_hole.py
@@ -4,11 +4,13 @@ class FoxInAHole():
     def __init__(self, n_holes, memory_size):
         self.n_holes = n_holes
         self.memory_size = memory_size
+        self.reward = 0
 
     def reset(self):
         # reset the environment to initial state
         self.done = [False, False] # first one stands for won, second one for lost
         self.fox = np.random.randint(1, self.n_holes+1)
+        self.reward = 0
         return self.done
 
     def step(self):
@@ -28,10 +30,10 @@ class FoxInAHole():
         # perform one guess in the game logic
         if action == self.fox:
             self.done = [True, False] # the game is won when the fox is found
-            reward = 1
+            self.reward += 1
         elif timestep == self.memory_size:
             self.done = [False, True] # the game is lost if the fox hasn't been found after the max amount of timesteps
-            reward = -10
+            self.reward -= 1
         else:
-            reward = 0
-        return reward, self.done
+            self.reward -= 1
+        return self.reward, self.done